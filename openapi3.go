5974be39ef94d2c679df39f15c7812710b74ef31
Tyler Brockmeyer <tjb101@yahoo.com>
2019-07-02 11:01:14 -0500

    Add swagger docs mounting, and security schemes

--- openapi3.go
+++ openapi3.go
@@ -2,38 +2,36 @@ package oas3
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/tjbrockmeyer/oas3models"
 	"io/ioutil"
 	"net/http"
 )
 
-type OpenAPI3 struct {
-	Doc *oas3models.OpenAPIDoc
-}
+type OpenAPI3 oas3models.OpenAPIDoc
 
 // Create a new specification for your API
 // This will create the endpoints in the documentation and will create routes for them.
 func NewOpenAPISpec3(title, description, version string, endpoints []*Endpoint, apiSubRouter *mux.Router) *OpenAPI3 {
 	spec := &OpenAPI3{
-		Doc: &oas3models.OpenAPIDoc{
-			OpenApi: "3.0.0",
-			Info: &oas3models.InfoDoc{
-				Title:       title,
-				Description: description,
-				Version:     version,
-			},
-			Servers: make([]*oas3models.ServerDoc, 0, 1),
-			Tags:    make([]*oas3models.TagDoc, 0, 3),
-			Paths:   make(oas3models.PathsDoc),
+		OpenApi: "3.0.0",
+		Info: &oas3models.InfoDoc{
+			Title:       title,
+			Description: description,
+			Version:     version,
 		},
+		Servers:    make([]*oas3models.ServerDoc, 0, 1),
+		Tags:       make([]*oas3models.TagDoc, 0, 3),
+		Paths:      make(oas3models.PathsDoc),
+		Components: &oas3models.ComponentsDoc{},
 	}
 	for _, e := range endpoints {
-		pathItem, ok := spec.Doc.Paths[e.settings.path]
+		pathItem, ok := spec.Paths[e.settings.path]
 		if !ok {
 			pathItem = &oas3models.PathItemDoc{
 				Methods: make(map[oas3models.HTTPVerb]*oas3models.OperationDoc)}
-			spec.Doc.Paths[e.settings.path] = pathItem
+			spec.Paths[e.settings.path] = pathItem
 		}
 		pathItem.Methods[oas3models.HTTPVerb(e.settings.method)] = e.Doc
 		apiSubRouter.Path(e.settings.path).Methods(string(e.settings.method)).HandlerFunc(e.run)
@@ -41,24 +39,9 @@ func NewOpenAPISpec3(title, description, version string, endpoints []*Endpoint, 
 	return spec
 }
 
-func (o *OpenAPI3) SwaggerDocs(route *mux.Route, projectSwaggerDir string) *OpenAPI3 {
-	path, err := route.GetPathTemplate()
-	if err != nil {
-		panic(err)
-	}
-	route.Handler(http.StripPrefix(path, http.FileServer(http.Dir(projectSwaggerDir))))
-
-	if b, err := json.Marshal(o.Doc); err != nil {
-		panic("could not marshal Open API 3 spec: " + err.Error())
-	} else if err = ioutil.WriteFile(projectSwaggerDir+"/spec.json", b, 0644); err != nil {
-		panic("could not write Open API 3 spec to " + projectSwaggerDir + ": " + err.Error())
-	}
-	return o
-}
-
 // Add a server to the API
 func (o *OpenAPI3) Server(url, description string) *OpenAPI3 {
-	o.Doc.Servers = append(o.Doc.Servers, &oas3models.ServerDoc{
+	o.Servers = append(o.Servers, &oas3models.ServerDoc{
 		Url:         url,
 		Description: description,
 	})
@@ -67,7 +50,7 @@ func (o *OpenAPI3) Server(url, description string) *OpenAPI3 {
 
 // Add a tag to the API with a description
 func (o *OpenAPI3) Tag(name, description string) *OpenAPI3 {
-	o.Doc.Tags = append(o.Doc.Tags, &oas3models.TagDoc{
+	o.Tags = append(o.Tags, &oas3models.TagDoc{
 		Name:        name,
 		Description: description,
 	})
@@ -75,10 +58,58 @@ func (o *OpenAPI3) Tag(name, description string) *OpenAPI3 {
 }
 
 // Add global security requirements for the API
-func (o *OpenAPI3) Security(name string, scopes ...string) *OpenAPI3 {
-	o.Doc.Security = append(o.Doc.Security, &oas3models.SecurityRequirementDoc{
+func (o *OpenAPI3) SecurityRequirement(name string, scopes ...string) *OpenAPI3 {
+	o.Security = append(o.Security, &oas3models.SecurityRequirementDoc{
 		Name:   name,
 		Scopes: scopes,
 	})
 	return o
+}
+
+func (o *OpenAPI3) NewAPIKey(name, description, headerName string) *OpenAPI3 {
+	if o.Components.SecuritySchemes == nil {
+		o.Components.SecuritySchemes = make(map[string]*oas3models.SecuritySchemeDoc)
+	}
+	o.Components.SecuritySchemes[name] = &oas3models.SecuritySchemeDoc{
+		Type: "apiKey",
+		In:   "header",
+		Name: headerName,
+	}
+	return o
+}
+
+func (o *OpenAPI3) NewClientCredentialsOAuth(
+	name, description, tokenUrl, refreshUrl string,
+	scopes map[string]string) *OpenAPI3 {
+	if o.Components.SecuritySchemes == nil {
+		o.Components.SecuritySchemes = make(map[string]*oas3models.SecuritySchemeDoc)
+	}
+	o.Components.SecuritySchemes[name] = &oas3models.SecuritySchemeDoc{
+		Type: "oauth2",
+		Flows: map[oas3models.OAuthFlowType]*oas3models.OAuthFlowDoc{
+			"clientCredentials": {
+				TokenUrl:   tokenUrl,
+				RefreshUrl: refreshUrl,
+				Scopes:     scopes,
+			},
+		},
+	}
+	fmt.Println(o.Components.SecuritySchemes[name])
+	return o
+}
+
+// Mount your local Swagger UI at the route specified by 'route'.
+func (o *OpenAPI3) SwaggerDocs(route *mux.Route, projectSwaggerDir string) (*OpenAPI3, error) {
+	path, err := route.GetPathTemplate()
+	if err != nil {
+		return o, fmt.Errorf("failed to get path for provided Swagger route: %s", err.Error())
+	}
+	route.Handler(http.StripPrefix(path, http.FileServer(http.Dir(projectSwaggerDir))))
+
+	if b, err := json.Marshal(o); err != nil {
+		return o, fmt.Errorf("could not marshal Open API 3 spec: %s", err.Error())
+	} else if err = ioutil.WriteFile(projectSwaggerDir+"/spec.json", b, 0644); err != nil {
+		return o, fmt.Errorf("could not write Open API 3 spec to %s: %s", projectSwaggerDir, err.Error())
+	}
+	return o, nil
 }
